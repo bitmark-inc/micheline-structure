4351f2ac8fb1f79ab810206790f070889d3c4fe4
hvthhien <hvthhien@gmail.com>
2023-05-16 17:33:31 +0700

    support packed prim

--- micheline.go
+++ micheline.go
@@ -5,6 +5,7 @@ import (
 	"reflect"
 
 	"blockwatch.cc/tzgo/micheline"
+	"blockwatch.cc/tzgo/tezos"
 )
 
 func Prim(data interface{}) micheline.Prim {
@@ -57,4 +58,55 @@ func Prim(data interface{}) micheline.Prim {
 	default:
 		return micheline.EmptyPrim
 	}
+}
+
+func PackedPrim(data interface{}) micheline.Prim {
+	return PackAll(Prim(data))
+}
+
+func PackAll(p micheline.Prim) micheline.Prim {
+	if p.Type == micheline.PrimString {
+		return Pack(p)
+	}
+	if p.LooksLikeCode() {
+		return p
+	}
+
+	pp := p
+	pp.Args = make([]micheline.Prim, len(p.Args))
+	for i, v := range p.Args {
+		pp.Args[i] = PackAll(v)
+	}
+	return pp
+}
+
+func Pack(p micheline.Prim) micheline.Prim {
+	if p.Type == micheline.PrimString {
+		a, err := tezos.ParseAddress(p.String)
+		if err == nil {
+			return micheline.NewBytes(a.EncodePadded())
+		}
+
+		k, err := tezos.ParseKey(p.String)
+		if err == nil {
+			return micheline.NewBytes(k.Bytes())
+		}
+
+		s, err := tezos.ParseSignature(p.String)
+		if err == nil {
+			if s.Type.Tag() == 255 {
+				return micheline.NewBytes(s.Bytes())
+			} else {
+				//remove tag byte prefix
+				return micheline.NewBytes(s.Bytes()[1:])
+			}
+		}
+
+		id, err := tezos.ParseChainIdHash(p.String)
+		if err == nil {
+			return micheline.NewBytes(id.Bytes())
+		}
+	}
+
+	return p
 }
